6baded20b5aab865a5510f08fb87b1bd42811f3c
Sulaymon <demirel6777@gmail.com>
2017-07-07 23:55:08 +0300

    Created MainActivity class and layout

---
+++ app/src/main/java/com/example/sulaymon/remindme/MainActivity.java
@@ -0,0 +1,18 @@
+package com.example.sulaymon.remindme;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
+/**
+ * Created by Sulaymon on 07.07.2017.
+ */
+
+public class MainActivity extends Activity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+}
